ebd836026db87596910bea931689a378cbf19eee
wangxu <wangxu831217@hotmail.com>
2017-09-21 22:35:15 +0800

    sd

--- pySrc/fileList.py
+++ pySrc/fileList.py
@@ -1,9 +1,9 @@
-#_*_coding:utf-8_*_
+# _*_coding:utf-8_*_
 import os
 
 def fileList():
     openDir = 'C:\\Users\\Administrator\\Desktop\\yinzheng\\before'
-    lfile=os.listdir(openDir)
+    lfile = os.listdir(openDir)
     
     for sfileName in lfile:
         print (sfileName)
