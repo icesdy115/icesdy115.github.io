e94b1eafcf2f4cd9e8f251008353ac5c3edcb664
icesdy115 <wangxu831217@hotmail.com>
2018-01-10 14:35:34 +0800

    icesdy115
    html files format

--- mdFile/md2html.bat
+++ mdFile/md2html.bat
@@ -1,7 +1,4 @@
 @echo off
 for /f  %%a in ('dir /a /b E:\github\icesdy115.github.io\mdFile\*.md') do ( 
-	echo %%a
     python md2html.py E:\github\icesdy115.github.io\mdFile\%%a  .\html\%%a.html
 )
-
-    
